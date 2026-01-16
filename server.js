fdfe6f86c9816369a082032719d3cda74e9fe058
heronimuseri <heronimuseri@gmail.com>
2026-01-16 12:15:29 +0700

    Increase upload limit for backup handling

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
